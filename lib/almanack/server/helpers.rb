0dfddd07296c27b7d4d0ff31f494056ce3507ea8
Phitherek_ <phitherek@gmail.com>
2017-11-08 22:39:40 +0100

    Fix feed url to not include port and use the correct scheme.

--- lib/almanack/server/helpers.rb
+++ lib/almanack/server/helpers.rb
@@ -3,7 +3,7 @@ module Almanack
     module Helpers
       # @return The URL to your consolidated iCal feed.
       def feed_url
-        "webcal://#{request.host}:#{request.port}/#{settings.feed_path}.ics"
+        "#{request.scheme}://#{request.host}#{request.port == 80 || request.port == 443 ? '' : ":#{request.port}"}/#{settings.feed_path}.ics"
       end
 
       # @return The URL to Almanack's project homepage.
